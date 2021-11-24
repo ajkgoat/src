cebdd6d974423963578af5e3b05e4b8098b7553f
ajkgoat <93507269+ajkgoat@users.noreply.github.com>
2021-11-24 17:24:02 +0545

    Add files via upload

---
+++ src/Menu/Categories.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Categories = ({ categories, filterItem }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            className="filter-btn"
+            key={index}
+            onClick={() => {
+              filterItem(category);
+            }}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Tabs/index.css";
+import App from "./Tabs/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
